78e39e8264b8272b9265d6e798a9dae19243a1a1
Atchuthan G <126309770+Atchuthan-G@users.noreply.github.com>
2023-10-05 12:30:03 +0530

    Delete backend/day 3 backend/fitness-tracker directory

--- backend/day 3 backend/fitness-tracker/src/main/java/com/example/demo/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.Users;
-
-public interface UserRepo extends JpaRepository<Users, Long> {
-    Users findByUsername(String username);
-}
